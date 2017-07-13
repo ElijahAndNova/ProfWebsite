da5b7deb259f61eca26b55ddaa996d5e122d45e9
Elijah Spiro <espiro18@amherst.edu>
2017-07-13 00:30:53 -0400

    Updating home page animation

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,9 @@
 $(document).ready((function() {
+// $(document).on('ready page:load', function() {
+// $(document).on("turbolinks:load", function() {
 
     var screenHeight = window.innerHeight;
-    $('.animate').css("margin-top", ((screenHeight-300)/2-225));
+    $('.animate').css("margin-top", ((screenHeight - 300) / 2 - 225));
 
     $('.ultimate').fadeIn(500);
 
@@ -10,18 +12,21 @@ $(document).ready((function() {
 
     var img = $(".animate"),
         width = img.get(0).width,
-        screenWidth = $(window).width()+2200,
+        screenWidth = $(window).width() + 1600,
+        // duration = 50000;
         duration = 25000;
-
-    function complete() {
-        img.css('left', 130);
-    }
+    // duration = 10000;
 
     function animatePlane() {
-        img.css("left", -width).animate({
-            "left": screenWidth
-        }, duration, animatePlane, complete());
+        img.css('opacity', 0);
+        img.fadeTo(500, 1);
+        img.css('margin-left', 400);
+        img.css("left", 0).animate({
+            left: screenWidth
+        }, duration, animatePlane);
     }
 
     animatePlane();
+// });
+//     });
 }));
