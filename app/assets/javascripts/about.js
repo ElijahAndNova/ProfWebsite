a1b3a022af8bcc826a18842d41cb97f3863b7f64
Elijah Spiro <espiro18@amherst.edu>
2017-07-18 14:44:53 -0400

    Swapped image for colosseum engine (added skybox); removed awards page (for now); added fade on about me page

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,6 +1,31 @@
 function fadeThemIn(){
     $('.panel-about').stop().fadeTo(750, 1);
     $('.panel-about2').stop().fadeTo(750, 1);
+    fadeInPic();
+    fadeInParagraph1();
+    // fadeInParagraph2();
+    // fadeInParagraph3();
+    // setTimeout(function() { fadeInPic() }, 1000);
+    // setTimeout(function() { fadeInParagraph1(); }, 1000);
+    setTimeout(function() { fadeInParagraph2(); }, 500);
+    setTimeout(function() { fadeInParagraph3(); }, 1000);
+}
+
+function fadeInPic(){
+    $('.pic_of_me_5').stop().fadeTo(500, 1);
+    $('.title').stop().fadeTo(500, 1);
+}
+
+function fadeInParagraph1(){
+    $('.p1').stop().fadeTo(2000, 1);
+}
+
+function fadeInParagraph2(){
+    $('.p2').stop().fadeTo(2000, 1);
+}
+
+function fadeInParagraph3(){
+    $('.p3').stop().fadeTo(2000, 1);
 }
 
 $(document).ready((function() {
