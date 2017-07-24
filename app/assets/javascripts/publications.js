e3081e498154c727a229619150f8266a46b44421
Elijah Spiro <espiro18@amherst.edu>
2017-07-24 14:50:49 -0400

    Attempted first mobile changes

--- app/assets/javascripts/publications.js
+++ app/assets/javascripts/publications.js
@@ -2,7 +2,7 @@ $(document).ready((function() {
     var screenHeight = window.innerHeight;
     $('.publication-panels').css("margin-top", ((screenHeight-300)/2-50));
 
-    $('.panel-publications0').stop().fadeTo(500, 1);
-    $('.panel-publications1').stop().fadeTo(500, 1);
-    $('.panel-publications2').stop().fadeTo(500, 1);
+    $('.panel-publications0').stop().fadeTo(1000, 1);
+    setTimeout(function() { $('.panel-publications1').stop().fadeTo(1000, 1); }, 500);
+    setTimeout(function() { $('.panel-publications2').stop().fadeTo(1000, 1); }, 500);
 }));
--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -3,7 +3,7 @@ $(document).ready((function() {
     var screenHeight = window.innerHeight;
     $('.panel-resume0').css("margin-top", ((screenHeight-300)/2-50));
 
-    $('.panel-resume0').stop().fadeTo(500, 1);
-    $('.panel-resume1').stop().fadeTo(500, 1);
+    $('.panel-resume0').stop().fadeTo(1000, 1);
+    setTimeout(function() { $('.panel-resume1').stop().fadeTo(1000, 1); }, 500);
 
 }));
