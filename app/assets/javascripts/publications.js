86c491314cc58effb9df58bf88b455ec27b5ccd6
Elijah Spiro <espiro18@amherst.edu>
2017-08-08 11:16:03 -0400

    Day two of optimizing for mobile camp

--- app/assets/javascripts/publications.js
+++ app/assets/javascripts/publications.js
@@ -1,6 +1,13 @@
 $(document).ready((function() {
     var screenHeight = window.innerHeight;
+    var screenWidth = window.innerWidth;
+
     $('.publication-panels').css("margin-top", ((screenHeight-300)/2-50));
+
+    if ( $('.global').css('display') == 'none' ) {
+        // $('.panel-publications0').css("left", 18);
+        $('.panel-publications0').css("width", screenWidth - 50);
+    }
 
     $('.panel-publications0').stop().fadeTo(1000, 1);
     setTimeout(function() { $('.panel-publications1').stop().fadeTo(1000, 1); }, 500);
--- app/assets/javascripts/research.js
+++ app/assets/javascripts/research.js
@@ -8,7 +8,16 @@ function fadeAllIn(){
 $(document).ready((function() {
 
     var screenHeight = window.innerHeight;
+    var screenWidth = window.innerWidth;
+
     $('.research-panels').css("margin-top", ((screenHeight-300)/2-125));
+
+    if ( $('.global').css('display') == 'none' ) {
+        $('.panel-research').css("width", screenWidth - 50);
+        $('.panel-research2').css("width", screenWidth - 50);
+        $('.panel-research3').css("width", screenWidth - 50);
+        $('.panel-research4').css("width", screenWidth - 50);
+    }
 
     fadeAllIn();
 
