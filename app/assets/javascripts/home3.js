c48643d13175210b933cdbdb79d50e125c877c89
Elijah Spiro <espiro18@amherst.edu>
2017-08-08 14:29:12 -0400

    Added particles to homepage

--- app/assets/javascripts/home3.js
+++ app/assets/javascripts/home3.js
@@ -14,16 +14,20 @@ $(document).ready((function() {
     // $('.hexagon').css("margin-left", ((screenWidth - 300) / 2+142-250));
     // $('.hexagon').css("margin-top", ((screenHeight - 300) / 2 - 180));
 
-    $('.hexpic').fadeTo(500, 1);
-    $('.hex1').fadeTo(500, 1);
-    $('.hex2').fadeTo(500, 1);
-    $('.hex3').fadeTo(500, 1);
-    $('.hex4').fadeTo(500, 1);
-    $('.hex5').fadeTo(500, 1);
-    $('.hex6').fadeTo(500, 1);
-    $('.hex7').fadeTo(500, 1);
-    $('.hex8').fadeTo(500, 1);
-    $('.hex9').fadeTo(500, 1);
-    $('.hex10').fadeTo(500, 1);
+    function loadAllIn(){
+        $('.hexpic').fadeTo(500, 1);
+        $('.hex1').fadeTo(500, 1);
+        $('.hex2').fadeTo(500, 1);
+        $('.hex3').fadeTo(500, 1);
+        $('.hex4').fadeTo(500, 1);
+        $('.hex5').fadeTo(500, 1);
+        $('.hex6').fadeTo(500, 1);
+        $('.hex7').fadeTo(500, 1);
+        $('.hex8').fadeTo(500, 1);
+        $('.hex9').fadeTo(500, 1);
+        $('.hex10').fadeTo(500, 1);
+    }
+
+    setTimeout(function() {loadAllIn();}, 500);
 
 }));
