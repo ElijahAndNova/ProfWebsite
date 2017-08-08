18f40aa061803c6506e9363480ee90fee653dab2
Elijah Spiro <espiro18@amherst.edu>
2017-08-08 10:19:53 -0400

    Day two of optimizing for mobile camp

--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -1,9 +1,15 @@
 $(document).ready((function() {
 
     var screenHeight = window.innerHeight;
+    var screenWidth = window.innerWidth;
+
     $('.panel-resume0').css("margin-top", ((screenHeight-300)/2-50));
+
+    $('.panel-resume0').css("right", 18);
+    $('.panel-resume0').css("width", screenWidth-50);
 
     $('.panel-resume0').stop().fadeTo(1000, 1);
     setTimeout(function() { $('.panel-resume1').stop().fadeTo(1000, 1); }, 500);
+
 
 }));
